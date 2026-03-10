3d4d2e685b6506716416e4cec35cb8b1aa3a1412
Chinmay Shivratriwar <shivratriwarchinmay@gmail.com>
2026-03-10 08:52:27 +0530

    [CNS] - config changes

---
+++ src/main/java/com/chinmayshivratriwar/nl_sql_engine/config/DataSourceConfig.java
@@ -0,0 +1,59 @@
+package com.chinmayshivratriwar.nl_sql_engine.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        log.info("=== DB CONNECTION DEBUG ===");
+        log.info("URL: {}", url);
+        log.info("USERNAME: {}", username);
+        log.info("PASSWORD is null: {}", password == null);
+        log.info("PASSWORD length: {}", password != null ? password.length() : 0);
+        log.info(password);
+        log.info("==========================");
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName("org.postgresql.Driver");
+        config.setMaximumPoolSize(5);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+        config.addDataSourceProperty("sslmode", "require");
+
+        log.info("Attempting to connect to database...");
+
+        try {
+            HikariDataSource dataSource = new HikariDataSource(config);
+            log.info("Database connected successfully!");
+            return dataSource;
+        } catch (Exception e) {
+            log.error("Database connection failed: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
